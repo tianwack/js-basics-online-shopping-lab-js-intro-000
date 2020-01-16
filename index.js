b78805c2cf884d64283f10f2f2a2e4e3ca251b94
Christian Wacker <christianwwacker@gmail.com>
2020-01-16 00:18:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function viewCart() {
 function total() {
   var total = 0;
   for(var i = 0; i < cart.length; i++) {
-    total = cart[i].itemPrice + total;
+    total += cart[i].itemPrice;
   }
   return total;
 }
