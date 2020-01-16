48fbf03170e192accc7b2b8e2628781a4582315b
Christian Wacker <christianwwacker@gmail.com>
2020-01-16 00:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var itemObj = {itemName:`${item}`, itemPrice: Math.floor(Math.random()*100)};
- getCart().push(itemObj);
+ cart.push(itemObj);
  return itemObj.itemName + " has been added to your cart."
 }
 
